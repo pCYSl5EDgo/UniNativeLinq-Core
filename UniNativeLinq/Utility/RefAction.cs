44c30feac884cb8cb423d57bee4b6195ea607868
pCYSl5EDgo <pcysl5edgo@yahoo.co.jp>
2019-06-16 13:35:13 +0900

    [add]RefFunc/RefAction

---
+++ UniNativeLinq/Utility/RefAction.cs
@@ -0,0 +1,7 @@
+﻿namespace UniNativeLinq
+{
+    delegate void RefAction<T0, T1>(ref T0 arg0, ref T1 arg1);
+    delegate void RefAction<T0, T1, T2>(ref T0 arg0, ref T1 arg1, ref T2 arg2);
+
+    delegate void RefSelectIndex<T0, T1>(ref T0 arg0, long index, ref T1 arg1);
+}
---
+++ UniNativeLinq/Utility/RefFunc.cs
@@ -0,0 +1,6 @@
+﻿namespace UniNativeLinq
+{
+    delegate TResult RefFunc<T0, out TResult>(ref T0 arg0);
+    
+    delegate bool RefWhereIndex<T0>(ref T0 arg0, long index);
+}
