1f65ee0e8d33597a18bb3dad5553229ca48dfc74
pCYSl5EDgo <pcysl5edgo@yahoo.co.jp>
2019-06-21 17:03:52 +0900

    [refactor]Rename Function 2 Func

--- CecilRewrite/Aggregate/AggregateFuncWithResultTypeHelper.cs
+++ CecilRewrite/Aggregate/AggregateFuncWithResultTypeHelper.cs
@@ -4,18 +4,16 @@ using Mono.Cecil;
 using Mono.Cecil.Cil;
 using Mono.Cecil.Rocks;
 
-// ReSharper disable InconsistentNaming
-
 namespace CecilRewrite
 {
     using static Program;
-    static class AggregateFunctionSmallHelper
+    static class AggregateFuncWithResultTypeHelper
     {
         private const string NameSpace = "UniNativeLinq";
 
         internal static void Create(ModuleDefinition module)
         {
-            var @static = new TypeDefinition(NameSpace, nameof(AggregateFunctionSmallHelper), StaticExtensionClassTypeAttributes, module.TypeSystem.Object);
+            var @static = new TypeDefinition(NameSpace, nameof(AggregateFuncWithResultTypeHelper), StaticExtensionClassTypeAttributes, module.TypeSystem.Object);
             @static.CustomAttributes.Add(ExtensionAttribute);
             module.Types.Add(@static);
             foreach (var type in module.Types.Where(x => x.IsValueType && x.IsPublic && x.HasInterfaces && x.Interfaces.Any(y => y.InterfaceType.Name == "IRefEnumerable`2")))
@@ -31,12 +29,19 @@ namespace CecilRewrite
                 DeclaringType = @static,
                 AggressiveInlining = true,
             };
+            var TResult0 = new GenericParameter("TResult0", method);
+            method.ReturnType = TResult0;
             method.CustomAttributes.Add(ExtensionAttribute);
+
             var added = method.FromTypeToMethodParam(type.GenericParameters);
             var @this = type.MakeGenericInstanceType(added);
+
             GenericParameter TAccumulate0;
             TAccumulate0 = new GenericParameter(nameof(TAccumulate0), method);
             method.GenericParameters.Add(TAccumulate0);
+
+            method.GenericParameters.Add(TResult0);
+
 
             var thisParam = new ParameterDefinition("this", ParameterAttributes.In, @this.MakeByReferenceType());
             thisParam.CustomAttributes.Add(IsReadOnlyAttribute);
@@ -46,55 +51,51 @@ namespace CecilRewrite
             seed = new ParameterDefinition(nameof(seed), ParameterAttributes.None, TAccumulate0.MakeByReferenceType());
             method.Parameters.Add(seed);
 
-            var funcReference = MainModule.ImportReference(SystemModule.GetType("System", "Func`3")).MakeGenericInstanceType(new[]
-            {
-                TAccumulate0,
-                @this.GetElementTypeOfCollectionType().Replace(method.GenericParameters),
-                TAccumulate0,
-            });
-            method.Parameters.Add(new ParameterDefinition("func", ParameterAttributes.None, funcReference));
+            var Enumerator = (GenericInstanceType)@this.GetEnumeratorTypeOfCollectionType().Replace(method.GenericParameters);
+            var Element = @this.GetElementTypeOfCollectionType().Replace(method.GenericParameters);
 
-            FillBody(@this, method, TAccumulate0, funcReference);
-            @static.Methods.Add(method);
-        }
+            var TFunc0 = MainModule.ImportReference(SystemModule.GetType("System", "Func`3")).MakeGenericInstanceType(TAccumulate0, Element, TAccumulate0);
+            var TResultFunc0 = MainModule.ImportReference(SystemModule.GetType("System", "Func`2")).MakeGenericInstanceType(TAccumulate0, TResult0);
+            method.Parameters.Add(new ParameterDefinition("func", ParameterAttributes.None, TFunc0));
+            method.Parameters.Add(new ParameterDefinition("resultFunc", ParameterAttributes.None, TResultFunc0));
 
-        private static void FillBody(GenericInstanceType @this, MethodDefinition method, GenericParameter accumulate0, GenericInstanceType func)
-        {
             var body = method.Body;
 
-            var EnumeratorType = (GenericInstanceType)@this.GetEnumeratorTypeOfCollectionType().Replace(method.GenericParameters);
-            var ElementType = @this.GetElementTypeOfCollectionType().Replace(method.GenericParameters);
-
-            body.Variables.Add(new VariableDefinition(EnumeratorType));
-            body.Variables.Add(new VariableDefinition(ElementType.MakeByReferenceType()));
-            body.Variables.Add(new VariableDefinition(MainModule.TypeSystem.Boolean));
+            body.Variables.Add(new VariableDefinition(Enumerator));
+            body.Variables.Add(new VariableDefinition(Element.MakeByReferenceType()));
+            body.Variables.Add(new VariableDefinition(@this.Module.TypeSystem.Boolean));
 
             var il0007 = Instruction.Create(OpCodes.Ldloca_S, body.Variables[0]);
-            var il002E = Instruction.Create(OpCodes.Ldloca_S, body.Variables[0]);
+            var il0025 = Instruction.Create(OpCodes.Ldloca_S, body.Variables[0]);
 
             var processor = body.GetILProcessor();
             processor.Do(OpCodes.Ldarg_0);
-            processor.Call(@this.FindMethod("GetEnumerator", Helper.NoParameter));
+            processor.GetEnumerator(@this);
             processor.Do(OpCodes.Stloc_0);
             processor.Append(il0007);
             processor.LdLocaS(2);
-            processor.Call(EnumeratorType.FindMethod("TryGetNext"));
+            processor.Call(Enumerator.FindMethod("TryGetNext"));
             processor.Do(OpCodes.Stloc_1);
             processor.Do(OpCodes.Ldloc_2);
-            processor.False(il002E);
+            processor.False(il0025);
             processor.Do(OpCodes.Ldarg_1);
             processor.Do(OpCodes.Ldarg_2);
             processor.Do(OpCodes.Ldarg_1);
-            processor.Append(Instruction.Create(OpCodes.Ldobj, accumulate0));
+            processor.LdObj(TAccumulate0);
             processor.Do(OpCodes.Ldloc_1);
-            processor.Append(Instruction.Create(OpCodes.Ldobj, ElementType));
-            var Func3 = MainModule.ImportReference(SystemModule.GetType("System", "Func`3")).MakeGenericInstanceType(accumulate0, ElementType, accumulate0);
-            processor.CallVirtual(Func3.FindMethod("Invoke"));
-            processor.Append(Instruction.Create(OpCodes.Stobj, accumulate0));
+            processor.LdObj(Element);
+            processor.CallVirtual(TFunc0.FindMethod("Invoke"));
+            processor.Append(Instruction.Create(OpCodes.Stobj, TAccumulate0));
             processor.Jump(il0007);
-            processor.Append(il002E);
-            processor.Call(EnumeratorType.FindMethod("Dispose", Helper.NoParameter));
+            processor.Append(il0025);
+            processor.Call(Enumerator.FindMethod("Dispose", Helper.NoParameter));
+            processor.Do(OpCodes.Ldarg_3);
+            processor.Do(OpCodes.Ldarg_1);
+            processor.LdObj(TAccumulate0);
+            processor.CallVirtual(TResultFunc0.FindMethod("Invoke"));
             processor.Ret();
+
+            @static.Methods.Add(method);
         }
     }
 }
