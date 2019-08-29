402311840c86219ae3241d1620a0e33a0d5ae0ba
pCYSl5EDgo <pcysl5edgo@yahoo.co.jp>
2019-08-29 22:06:15 +0900

    [update]Naming Tuple of WithIndex

--- UniNativeLinq/Enumerable/SelectIndex/WithIndex.cs
+++ UniNativeLinq/Enumerable/SelectIndex/WithIndex.cs
@@ -2,10 +2,10 @@
 {
     public readonly struct
         WithIndex<T>
-        : ISelectIndex<T, (T, long)>
+        : ISelectIndex<T, (T element, long index)>
         where T : unmanaged
     {
-        public void Execute(ref T source, long index, ref (T, long) result)
+        public void Execute(ref T source, long index, ref (T element, long index) result)
         {
             result.Item1 = source;
             result.Item2 = index;
