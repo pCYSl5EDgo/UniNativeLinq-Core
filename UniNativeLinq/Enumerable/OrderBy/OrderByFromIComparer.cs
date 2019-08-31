9d721511897cc74b7bbda6e96332738f2b4e599e
pCYSl5EDgo <pcysl5edgo@yahoo.co.jp>
2019-08-31 17:01:34 +0900

    [update]

--- UniNativeLinq/Enumerable/OrderBy/OrderByFromIComparer.cs
+++ UniNativeLinq/Enumerable/OrderBy/OrderByFromIComparer.cs
@@ -5,8 +5,8 @@ namespace UniNativeLinq
     public struct OrderByFromIComparer<T>
          : IRefFunc<T, T, int>
     {
-        private IComparer<T> comparer;
-        public OrderByFromIComparer(IComparer<T> comparer) => this.comparer = comparer;
-        public int Calc(ref T arg0, ref T arg1) => comparer.Compare(arg0, arg1);
+        public IComparer<T> Func;
+        public OrderByFromIComparer(IComparer<T> comparer) => this.Func = comparer;
+        public int Calc(ref T arg0, ref T arg1) => Func.Compare(arg0, arg1);
     }
 }
