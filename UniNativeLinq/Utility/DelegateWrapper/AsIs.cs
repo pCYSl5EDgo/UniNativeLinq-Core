ece252b25c54a5234bd263f14e17dc2f469cf86a
pcysl5edgo <pcysl5edgo@gmail.com>
2019-10-05 15:13:29 +0900

    [update]multiwork AsIs

--- UniNativeLinq/Utility/DelegateWrapper/AsIs.cs
+++ UniNativeLinq/Utility/DelegateWrapper/AsIs.cs
@@ -1,7 +1,8 @@
 ﻿namespace UniNativeLinq
 {
-    public struct AsIs<T0> : IRefAction<T0, T0>
+    public struct AsIs<T0> : IRefAction<T0, T0>, IRefFunc<T0, T0>
     {
+        public T0 Calc(ref T0 arg0) => arg0;
         public void Execute(ref T0 arg0, ref T0 arg1) => arg1 = arg0;
     }
 }
