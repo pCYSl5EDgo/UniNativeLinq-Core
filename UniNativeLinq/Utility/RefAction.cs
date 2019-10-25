7f9e437a4d22f60acc1974209ab4bf118e67b4a5
pcysl5edgo <pcysl5edgo@gmail.com>
2019-10-26 01:16:50 +0900

    [add]IForEachIndex

--- UniNativeLinq/Utility/RefAction.cs
+++ UniNativeLinq/Utility/RefAction.cs
@@ -5,4 +5,6 @@
     public delegate void RefAction<T0, T1, T2>(ref T0 arg0, ref T1 arg1, ref T2 arg2);
 
     public delegate void RefSelectIndex<T0, T1>(ref T0 arg0, long index, ref T1 arg1);
+
+    public delegate void RefForEachIndex<T0>(ref T0 arg0, long index);
 }
